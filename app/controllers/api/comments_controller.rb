4e2d943cb86b62d21efb69a9a0c43b5b02355568
giacoletti <giacoletti92@gmail.com>
2022-02-17 12:01:00 +0100

    Add sad path to request spec comment create action: user can't create a comment if the article has not been found can't create a comment if the comment param is missing can't create a comment if the comment body is empty anonymous user can't create a comment Implement logic in comments controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,5 +1,35 @@
 class Api::CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+  before_action :validate_params_presence, only: [:create]
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404_error
+
   def create
-    # binding.pry
+    article = Article.find(params[:article_id])
+    comment = current_user.comments.create(comment_params.merge(article: article))
+    if comment.persisted?
+      render json: { comment: comment }, status: :created
+    end
+  end
+
+  private
+
+  def validate_params_presence
+    if params[:comment].nil?
+      render_error('Comment param is missing', :unprocessable_entity)
+    elsif params[:comment][:body].nil? || params[:comment][:body] == ''
+      render_error("Comment body can't be empty", :unprocessable_entity)
+    end
+  end
+
+  def render_error(message, status)
+    render json: { message: message }, status: status
+  end
+
+  def render_404_error
+    render json: { message: 'Article not found' }, status: :not_found
+  end
+
+  def comment_params 
+    params[:comment].permit(:body)
   end
 end
--- spec/requests/api/article/comment/create_spec.rb
+++ spec/requests/api/article/comment/create_spec.rb
@@ -1,11 +1,11 @@
 RSpec.describe 'POST /api/articles/:id/comments', type: :request do
-  subject { response } 
+  subject { response }
 
   let(:user) { create(:user) }
   let(:credentials) { user.create_new_auth_token }
   let!(:article) { create(:article) }
 
-  describe 'As an authenticated user' do
+  describe 'as an authenticated user' do
     describe 'successfully' do
       before do
         post "/api/articles/#{article.id}/comments", params: {
@@ -14,13 +14,72 @@ RSpec.describe 'POST /api/articles/:id/comments', type: :request do
           }
         }, headers: credentials
       end
-
+      
       it { is_expected.to have_http_status :created }
-
+      
       it 'is expected to respond with saved comment' do
-        binding.pry
         expect(response_json['comment']['body']).to eq 'What an interesting article!'
       end
+    end
+    
+    describe 'unsuccessfully' do
+      describe 'due to invalid article id' do
+        before do
+          post "/api/articles/ASOIJDSAOIJ/comments", params: {
+            comment: {
+              body: 'What an interesting article!'
+            }
+          }, headers: credentials
+        end
+
+        it { is_expected.to have_http_status :not_found }
+        
+        it 'is expected to respond with an error message' do
+          expect(response_json['message']).to eq 'Article not found'
+        end
+      end
+
+      describe 'due to missing comment param' do
+        before do
+          post "/api/articles/#{article.id}/comments", params: {}, headers: credentials
+        end
+
+        it { is_expected.to have_http_status :unprocessable_entity }
+
+        it 'is expected to respond with an error message' do
+          expect(response_json['message']).to eq 'Comment param is missing'
+        end
+      end
+
+      describe 'due to empty comment body param' do
+        before do
+          post "/api/articles/#{article.id}/comments", params: {
+            comment: { body: '' }
+          }, headers: credentials
+        end
+
+        it { is_expected.to have_http_status :unprocessable_entity }
+
+        it 'is expected to respond with an error message' do
+          expect(response_json['message']).to eq "Comment body can't be empty"
+        end
+      end
+    end
+  end
+
+  describe 'as an anonymous user' do
+    before do
+      post "/api/articles/#{article.id}/comments", params: {
+        comment: {
+          body: 'What an interesting article!'
+        }
+      }, headers: nil
+    end
+
+    it { is_expected.to have_http_status :unauthorized }
+
+    it 'is expected to respond with an error message' do
+      expect(response_json['errors'].first).to eq 'You need to sign in or sign up before continuing.'
     end
   end
 end
